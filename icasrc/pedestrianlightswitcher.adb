11d158a048a9abc3b5bb8de2c07c90e3958b288b
Benjamin HAYWARD <p4117178@linux32.scm.tees.ac.uk>
2017-05-31 18:58:23 +0100

    Just updated pedestrianswitch to be more optimized

--- icasrc/pedestrianlightswitcher.adb
+++ icasrc/pedestrianlightswitcher.adb
@@ -1,21 +1,25 @@
 with HWIF;use HWIF;
-with HWIF_Types; use HWIF_Types;
+
 procedure PedestrianLightSwitcher (dir : in Direction) is
 begin
-
    if Pedestrian_Light = 1 then
-
-        SafeCrossingGuard:
-   	while((Traffic_Light(North) /= 4)&
-              (Traffic_Light(South) /= 4)&
-   	      (Traffic_Light(East)  /= 4)&
-              (Traffic_Light(West)  /= 4))
-	loop
- 	     delay 1.0; --Awaiting all lights to be red.
-      			--May need to call lights to switch to red here.
+        SafeCrossingGuard:  	
+	loop         
+             if ((Traffic_Light(North) = 4)&
+       	         (Traffic_Light(South) = 4)&
+   	         (Traffic_Light(East)  = 4)&
+                 (Traffic_Light(West)  = 4)) then
+       		 exit SafeCrossingGuard;
+            end if
+            delay 1.0; --Awaiting all lights to be red.
+         	Put_Line("Awaiting all lights being red.") 
    	end loop SafeCrossingGuard;
 
       Pedestrian_Light(dir) := 0 --Light off. Walk.
       --Check criteria; may just need to turn all lights off as all lights should be red at this point.
-   end if;
+   elsif Pedestrian_Light = 0 then
+      delay 5.0 --Check this delay in the criteria; this is the time peds are allowed to walk.
+      Pedestrian_Light(dir) := 1 --Light on
+   end if;`
 end;
+
